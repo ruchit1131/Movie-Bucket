b302ec8d27980fae43ec4abefaf3b320cdc0ae25
Ruchit Karnawat <ruchitkarnawat1999@gmail.com>
2021-01-23 23:08:37 +0530

    Update Youtube.js

--- client/src/components/pages/Youtube.js
+++ client/src/components/pages/Youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyB0g1FKb5pLOvChNR465pClWdmvS2GkeRc';
+const KEY = '<youtube key>';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
